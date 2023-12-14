38ef70557e8e8e72f95882c0de86fb0f8ea55d14
Yashas <ranjithacharya681@gmail.com>
2023-12-14 11:08:10 +0530

    added

---
+++ Project 1/commands.sh
@@ -0,0 +1,37 @@
+Jenkins CI/CD Pipeline - SonarQube, Docker, Github Webhooks on AWS
+
+https://www.youtube.com/watch?v=361bfIvXMBI
+
+Push the code into github
+
+Create a Three EC2 Machine
+Jenkins & Docker t2 Micro
+SonarQube t2 Medium
+Allow all the ports
+
+
+
+Jenkins
+sudo apt update -y
+sudo apt install openjdk-17-jre -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+sudo systemctl status jenkins
+
+Copy the IP and Login to the Jenkins
+
+New Item = Automated-Pipeline
+Free style Project
+Source code management= add git url | Main | Github hook trigger
+Goto Github repo = Webhook = url/github-webhook/
+Send everything
+Build Now
+
+
+
+
