90b7e7a484fea366e83712151fb1ece7549aba49
ranjit681 <ranjithacharya681@gmail.com>
2023-12-15 11:57:33 +0530

    added new code

--- Project 1/commands.sh
+++ Project 1/commands.sh
@@ -79,6 +79,12 @@ Docker
 sudo hostnamectl set-hostname docker
 /bin/bash
 sudo apt update -y
+sudo apt install docker.io -y
+sudo systemctl status docker
+sudo systemctl start docker
+sudo usermod -aG docker ubuntu
+
+
 
 
 
