2eacdf92f41db9568380629dcbca288e606accaa
ranjit681 <ranjithacharya681@gmail.com>
2023-12-15 12:18:12 +0530

    added new code

--- Project 1/commands.sh
+++ Project 1/commands.sh
@@ -118,7 +118,11 @@ COPY . /usr/share/nginx/html/
 Save it
 
 Automated-Pipeline/configure
-Post Build Actions : Remove the remote shell | Execute Shell: 
+Post Build Actions : Remove the remote shell | Execute Shell: scp ./* ubuntu @DIP:~/website/ 
+Save
+
+
+D-> Exit | ls | mkdir website | pwd | /home/ubuntu/website
 
 
 
